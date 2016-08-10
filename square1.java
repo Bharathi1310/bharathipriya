509390e0586102da9e45b1487bb257e0bf83a342
Bharathi1310 <bharathikalai1310@gmail.com>
2016-08-10 04:01:16 -0700

    Create square1.java

---
+++ square1.java
@@ -0,0 +1,18 @@
+
+public class square1 {
+
+	public static void main(String[] args) {
+		int a[]={1,2,3,4,5,5};
+		int count=0;
+		for(int i=0;i<a.length;i++){
+			for(int j=i+1;j<a.length;j++){
+				if(a[i]==a[j]){
+				count++;
+					System.out.println(count);
+				}
+			    }
+		       }
+
+	           }
+
+               }
